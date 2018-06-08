100e54e7454d61656b44b3fc90f84de296f91d01
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-08 09:57:48 -0400

    la app es capaz de obtener data del slider, calcular puntuación, mostrar mensaje, mostrar rondas, restar y actualizar valores

--- theAimForThat/ViewController.swift
+++ theAimForThat/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue : Int = 0
+    var targetValue : Int = 0
+    var roundsValue : Int = 0
+    var scoreValue : Int = 0
+    var differenceValue : Int = 0
+    var point : Int = 0
 
+    @IBOutlet weak var scoreLabels: UILabel!
+    @IBOutlet weak var roundsLabel: UILabel!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nuevaRonda()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,16 +34,54 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hola Mundo", message: "Ete mensaje es de la descripcion de la alerta", preferredStyle: .alert)
+        
+        self.differenceValue = Int((self.currentValue - self.targetValue).magnitude)
+        
+        self.point = differenceValue > 0 ? 100 - differenceValue : 1000
+        
+        self.scoreValue += self.point
+        
+        let mensaje = """
+        tu puntaje es \(self.point)
+        """
+        
+        let alert = UIAlertController(title: "Puntuacion", message: mensaje, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "genial!", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true)
+        
+        nuevaRonda()
     }
     @IBAction func sliderPoint(_ sender: UISlider) {
-        print("El valor del slide es: \(sender.value)")
+        self.currentValue = lroundf(sender.value)
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        resetValues()
+    }
+    func nuevaRonda(){
+        self.targetValue = Int(arc4random_uniform(100) + 1)
+        self.currentValue = 50
+        self.slider.value = Float(self.currentValue)
+        self.roundsValue += 1
+        actualizarLabels()
+    }
+    
+    func actualizarLabels(){
+        self.targetLabel.text = String(self.targetValue)
+        self.roundsLabel.text = String(self.roundsValue)
+        self.scoreLabels.text = String(self.scoreValue)
+    }
+    
+    func resetValues(){
+        self.roundsValue = 0
+        self.scoreValue = 0
+        self.targetValue = Int(arc4random_uniform(100) + 1)
+        self.currentValue = 50
+        actualizarLabels()
     }
     
 }
